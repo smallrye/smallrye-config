dcc4441613b8492fb97f20b59cbd0308d96bb18c
Simon Woodman <swoodman@redhat.com>
2018-06-14 16:51:54 +0100

    Initial version of MicroProfile ConfigSource for Zookeeper
    Changed package name
    
    Tidied up pom and additions to .gitignore
    
    Misc fixes
    
    Fixed typo
    
    Added Arquillian Tests
    
    Changed unit tests to use AssertJ
    
    Better handling of lack of configuration
    
    reformatting
    
    Added example
    
    Docs
    
    Added checked exception when lack of config
    
    Moving repo to SmallRye
    
    Addressing code review comments. Changed package names and improved imports in pom
    
    Uses AtomicReference for better thread safety
    
    first commit
    
    Initial version of MicroProfile ConfigSource for Zookeeper
    
    Changed package name
    
    Tidied up pom and additions to .gitignore
    
    Misc fixes
    
    Fixed typo
    
    Added Arquillian Tests
    
    Changed unit tests to use AssertJ
    
    Better handling of lack of configuration
    
    reformatting
    
    Added example
    
    Docs
    
    Fixing Github's non-inclusion of .adoc includes
    
    Fixed capitalisation of filename
    
    Added checked exception when lack of config
    
    Moving repo to SmallRye
    
    Addressing code review comments. Changed package names and improved imports in pom
    
    Uses AtomicReference for better thread safety
    
    Re-adding files that got removed during incorrect merge
    
    Renamed test package
    
    Renamed test package
    
    Changed logging and added to parent's maven build
    
    Reformatting comment to get javadoc to generate

---
+++ config-sources/zookeeper/example/src/main/java/io/smallrye/configsource/example/rest/RestApplication.java
@@ -0,0 +1,8 @@
+package io.smallrye.configsource.example.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class RestApplication extends Application {
+}
