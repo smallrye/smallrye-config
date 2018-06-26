b1893f78918575dd403a0830ef8f28bb14fec5ec
Bob McWhirter <bob@mcwhirter.org>
2018-06-26 16:56:06 -0400

    Ensure that converts are serializable.

--- implementation/src/main/java/io/smallrye/config/SmallRyeConfigBuilder.java
+++ implementation/src/main/java/io/smallrye/config/SmallRyeConfigBuilder.java
@@ -203,7 +203,15 @@ public class SmallRyeConfigBuilder implements ConfigBuilder {
         Collections.sort(sources, new Comparator<ConfigSource>() {
             @Override
             public int compare(ConfigSource o1, ConfigSource o2) {
-                return o2.getOrdinal() -  o1.getOrdinal();
+                int v1 = o1.getOrdinal();
+                int v2 = o2.getOrdinal();
+                if ( v1 > v2 ) {
+                    return -11;
+                }
+                if ( v1 < v2 ) {
+                    return +1;
+                }
+                return 0;
             }
         });
 
