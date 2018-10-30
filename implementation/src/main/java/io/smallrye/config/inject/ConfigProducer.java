9e1adc57c0a72a74e09bbf981400560e5f2f7860
Gunnar Hilling <gunnar@hilling.de>
2018-10-30 07:51:47 +0100

    extract logic from cdi producer

--- implementation/src/main/java/io/smallrye/config/inject/ConfigProducer.java
+++ implementation/src/main/java/io/smallrye/config/inject/ConfigProducer.java
@@ -16,29 +16,18 @@
 
 package io.smallrye.config.inject;
 
-import static io.smallrye.config.SecuritySupport.getContextClassLoader;
-
-import java.io.Serializable;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
+import java.io.Serializable;
+import java.util.*;
 
-import org.eclipse.microprofile.config.Config;
-import org.eclipse.microprofile.config.ConfigProvider;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
-import io.smallrye.config.SmallRyeConfig;
-import io.smallrye.config.StringUtil;
+import static io.smallrye.config.SecuritySupport.getContextClassLoader;
 
 /**
  * CDI producer for {@link Config} bean.
@@ -57,152 +46,55 @@ public class ConfigProducer implements Serializable{
     @Dependent
     @Produces @ConfigProperty
     String produceStringConfigProperty(InjectionPoint ip) {
-        return getValue(ip, String.class);
+        return ConfigProducerUtil.getValue(ip, String.class, getConfig(ip));
     }
 
     @Dependent
     @Produces @ConfigProperty
     Long getLongValue(InjectionPoint ip) {
-        return getValue(ip, Long.class);
+        return ConfigProducerUtil.getValue(ip, Long.class, getConfig(ip));
     }
 
     @Dependent
     @Produces @ConfigProperty
     Integer getIntegerValue(InjectionPoint ip) {
-        return getValue(ip, Integer.class);
+        return ConfigProducerUtil.getValue(ip, Integer.class, getConfig(ip));
     }
 
     @Dependent
     @Produces @ConfigProperty
     Float produceFloatConfigProperty(InjectionPoint ip) {
-        return getValue(ip, Float.class);
+        return ConfigProducerUtil.getValue(ip, Float.class, getConfig(ip));
     }
 
     @Dependent
     @Produces @ConfigProperty
     Double produceDoubleConfigProperty(InjectionPoint ip) {
-        return getValue(ip, Double.class);
+        return ConfigProducerUtil.getValue(ip, Double.class, getConfig(ip));
     }
 
     @Dependent
     @Produces @ConfigProperty
     Boolean produceBooleanConfigProperty(InjectionPoint ip) {
-        return getValue(ip, Boolean.class);
+        return ConfigProducerUtil.getValue(ip, Boolean.class, getConfig(ip));
     }
 
-    @SuppressWarnings("unchecked")
     @Dependent
     @Produces @ConfigProperty
     <T> Optional<T> produceOptionalConfigValue(InjectionPoint injectionPoint) {
-        Type type = injectionPoint.getAnnotated().getBaseType();
-        final Class<T> valueType;
-        if (type instanceof ParameterizedType) {
-            ParameterizedType parameterizedType = (ParameterizedType) type;
-
-            Type[] typeArguments = parameterizedType.getActualTypeArguments();
-            valueType = unwrapType(typeArguments[0]);
-        } else {
-            valueType = (Class<T>) String.class;
-        }
-        return Optional.ofNullable(getValue(injectionPoint, valueType));
+        return ConfigProducerUtil.optionalConfigValue(injectionPoint, getConfig(injectionPoint));
     }
 
     @Dependent
     @Produces @ConfigProperty
     <T> Set<T> producesSetConfigPropery(InjectionPoint ip) {
-        Type type = ip.getAnnotated().getBaseType();
-        final Class<T> valueType;
-        if (type instanceof ParameterizedType) {
-            ParameterizedType parameterizedType = (ParameterizedType) type;
-
-            Type[] typeArguments = parameterizedType.getActualTypeArguments();
-            valueType = unwrapType(typeArguments[0]);
-        } else {
-            valueType = (Class<T>) String.class;
-        }
-        HashSet<T> s = new HashSet<>();
-        String stringValue = getValue(ip, String.class);
-        String[] split = StringUtil.split(stringValue);
-        Config config = getConfig(ip);
-        for(int i = 0 ; i < split.length ; i++) {
-            T item = ((SmallRyeConfig) config).convert(split[i], valueType);
-            s.add(item);
-        }
-        return s;
+        return ConfigProducerUtil.setConfigProperty(ip, getConfig(ip));
     }
 
     @Dependent
     @Produces @ConfigProperty
     <T> List<T> producesListConfigPropery(InjectionPoint ip) {
-        Type type = ip.getAnnotated().getBaseType();
-        final Class<T> valueType;
-        if (type instanceof ParameterizedType) {
-            ParameterizedType parameterizedType = (ParameterizedType) type;
-
-            Type[] typeArguments = parameterizedType.getActualTypeArguments();
-            valueType = unwrapType(typeArguments[0]);
-        } else {
-            valueType = (Class<T>) String.class;
-        }
-        ArrayList<T> s = new ArrayList<>();
-        String stringValue = getValue(ip, String.class);
-        String[] split = StringUtil.split(stringValue);
-        Config config = getConfig(ip);
-        for(int i = 0 ; i < split.length ; i++) {
-            T item = ((SmallRyeConfig) config).convert(split[i], valueType);
-            s.add(item);
-        }
-        return s;
-    }
-
-    private <T> Class<T> unwrapType(Type type) {
-        if (type instanceof ParameterizedType) {
-            type = ((ParameterizedType) type).getActualTypeArguments()[0];
-        }
-        return (Class<T>) type;
-    }
-
-    private <T> T getValue
-            (InjectionPoint injectionPoint, Class<T> target) {
-        Config config = getConfig(injectionPoint);
-        String name = getName(injectionPoint);
-        try {
-            if (name == null) {
-                return null;
-            }
-            Optional<T> optionalValue = config.getOptionalValue(name, target);
-            if (optionalValue.isPresent()) {
-                return optionalValue.get();
-            } else {
-                String defaultValue = getDefaultValue(injectionPoint);
-                if (defaultValue != null && !defaultValue.equals(ConfigProperty.UNCONFIGURED_VALUE)) {
-                    return ((SmallRyeConfig)config).convert(defaultValue, target);
-                } else {
-                    return null;
-                }
-            }
-        } catch (RuntimeException e) {
-            return null;
-        }
-    }
-
-    private String getName(InjectionPoint injectionPoint) {
-        for (Annotation qualifier : injectionPoint.getQualifiers()) {
-            if (qualifier.annotationType().equals(ConfigProperty.class)) {
-                ConfigProperty configProperty = ((ConfigProperty)qualifier);
-                return ConfigExtension.getConfigKey(injectionPoint, configProperty);
-            }
-        }
-        return null;
-    }
-
-    private String getDefaultValue(InjectionPoint injectionPoint) {
-        for (Annotation qualifier : injectionPoint.getQualifiers()) {
-            if (qualifier.annotationType().equals(ConfigProperty.class)) {
-                return ((ConfigProperty) qualifier).defaultValue();
-            }
-        }
-        return null;
+        return ConfigProducerUtil.listConfigProperty(ip, getConfig(ip));
     }
 
 }
