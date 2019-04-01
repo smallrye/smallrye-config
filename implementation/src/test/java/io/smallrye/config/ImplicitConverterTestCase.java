f1fb99186b5891979e33bd5dd7855237a73c9034
Jeff Mesnil <jmesnil@gmail.com>
2019-04-01 09:16:20 +0200

    [test] add test for LocalDate converter

--- implementation/src/test/java/io/smallrye/config/ImplicitConverterTestCase.java
+++ implementation/src/test/java/io/smallrye/config/ImplicitConverterTestCase.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.net.URL;
+import java.time.LocalDate;
 
 import org.eclipse.microprofile.config.Config;
 import org.junit.Test;
@@ -19,6 +20,18 @@ public class ImplicitConverterTestCase {
         assertEquals("https", url.getProtocol());
         assertEquals("github.com", url.getHost());
         assertEquals("/smallrye/smallrye-config/", url.getPath());
+    }
+
+
+    @Test
+    public void testImplicitLocalDateConverter() {
+        Config config = buildConfig(
+                "my.date", "2019-04-01");
+        LocalDate date = config.getValue("my.date", LocalDate.class);
+        assertNotNull(date);
+        assertEquals(2019, date.getYear());
+        assertEquals(4, date.getMonthValue());
+        assertEquals(1, date.getDayOfMonth());
     }
 
     private static Config buildConfig(String... keyValues) {
