fb02864ea7c3e49a6023cc8208656817942e143c
Jeff Mesnil <jmesnil@gmail.com>
2019-06-28 14:12:40 +0200

    add Character converter

--- implementation/src/main/java/io/smallrye/config/Converters.java
+++ implementation/src/main/java/io/smallrye/config/Converters.java
@@ -94,6 +94,17 @@ public final class Converters {
         }
     });
 
+    @SuppressWarnings("unchecked")
+    static final Converter<Character> CHARACTER_CONVERTER = BuiltInConverter.of(11, (Converter & Serializable) value -> {
+        if (value != null) {
+            if (value.length() == 1) {
+                return Character.valueOf(value.charAt(0));
+            }
+            throw new IllegalArgumentException(value + " can not be converted to a Character");
+        }
+        return null;
+    });
+
     static final Map<Type, Converter<?>> ALL_CONVERTERS = new HashMap<>();
 
     static {
@@ -120,6 +131,10 @@ public final class Converters {
         ALL_CONVERTERS.put(OptionalInt.class, OPTIONAL_INT_CONVERTER);
         ALL_CONVERTERS.put(OptionalLong.class, OPTIONAL_LONG_CONVERTER);
         ALL_CONVERTERS.put(OptionalDouble.class, OPTIONAL_DOUBLE_CONVERTER);
+
+        ALL_CONVERTERS.put(Character.class, CHARACTER_CONVERTER);
+        ALL_CONVERTERS.put(Character.TYPE, CHARACTER_CONVERTER);
+
     }
 
     /**
@@ -195,6 +210,7 @@ public final class Converters {
                 case 8: return OPTIONAL_LONG_CONVERTER;
                 case 9: return OPTIONAL_DOUBLE_CONVERTER;
                 case 10: return INET_ADDRESS_CONVERTER;
+                case 11: return CHARACTER_CONVERTER;
                 default: throw new InvalidObjectException("Unknown converter ID");
             }
         }
