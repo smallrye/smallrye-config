9c767a9a5053b64e97c389297aec8da4593e0c1d
Roberto Cortez <radcortez@yahoo.com>
2020-05-14 13:52:58 +0100

    Fixes #309. Support escapes code in property expansion. (#312)
    * Removed redundant casts.
    
    * Fixes #309. Support escapes code in property expansion.

--- implementation/src/test/java/io/smallrye/config/ExpressionConfigSourceInterceptorTest.java
+++ implementation/src/test/java/io/smallrye/config/ExpressionConfigSourceInterceptorTest.java
@@ -5,27 +5,26 @@ import static org.junit.Assert.assertThrows;
 
 import java.util.NoSuchElementException;
 
-import org.eclipse.microprofile.config.Config;
 import org.junit.Test;
 
 public class ExpressionConfigSourceInterceptorTest {
     @Test
     public void simpleExpression() {
-        SmallRyeConfig config = (SmallRyeConfig) buildConfig("my.prop", "1234", "expression", "${my.prop}");
+        SmallRyeConfig config = buildConfig("my.prop", "1234", "expression", "${my.prop}");
 
         assertEquals("1234", config.getValue("expression", String.class));
     }
 
     @Test
     public void multipleExpressions() {
-        SmallRyeConfig config = (SmallRyeConfig) buildConfig("my.prop", "1234", "expression", "${my.prop}${my.prop}");
+        SmallRyeConfig config = buildConfig("my.prop", "1234", "expression", "${my.prop}${my.prop}");
 
         assertEquals("12341234", config.getValue("expression", String.class));
     }
 
     @Test
     public void composedExpressions() {
-        SmallRyeConfig config = (SmallRyeConfig) buildConfig("my.prop", "1234", "expression", "${${compose}}", "compose",
+        SmallRyeConfig config = buildConfig("my.prop", "1234", "expression", "${${compose}}", "compose",
                 "my.prop");
 
         assertEquals("1234", config.getValue("expression", String.class));
@@ -33,35 +32,35 @@ public class ExpressionConfigSourceInterceptorTest {
 
     @Test
     public void defaultExpression() {
-        SmallRyeConfig config = (SmallRyeConfig) buildConfig("expression", "${my.prop:1234}");
+        SmallRyeConfig config = buildConfig("expression", "${my.prop:1234}");
 
         assertEquals("1234", config.getValue("expression", String.class));
     }
 
     @Test
     public void defaultExpressionEmpty() {
-        SmallRyeConfig config = (SmallRyeConfig) buildConfig("expression", "12${my.prop:}34");
+        SmallRyeConfig config = buildConfig("expression", "12${my.prop:}34");
 
         assertEquals("1234", config.getValue("expression", String.class));
     }
 
     @Test
     public void defaultExpressionComposed() {
-        SmallRyeConfig config = (SmallRyeConfig) buildConfig("expression", "${my.prop:${compose}}", "compose", "1234");
+        SmallRyeConfig config = buildConfig("expression", "${my.prop:${compose}}", "compose", "1234");
 
         assertEquals("1234", config.getValue("expression", String.class));
     }
 
     @Test
     public void defaultExpressionComposedEmpty() {
-        SmallRyeConfig config = (SmallRyeConfig) buildConfig("expression", "${my.prop:${compose:}}", "my.prop", "1234");
+        SmallRyeConfig config = buildConfig("expression", "${my.prop:${compose:}}", "my.prop", "1234");
 
         assertEquals("1234", config.getValue("expression", String.class));
     }
 
     @Test
     public void noExpression() {
-        SmallRyeConfig config = (SmallRyeConfig) buildConfig("expression", "${my.prop}");
+        SmallRyeConfig config = buildConfig("expression", "${my.prop}");
 
         final NoSuchElementException exception = assertThrows(NoSuchElementException.class,
                 () -> config.getValue("expression", String.class));
@@ -70,7 +69,7 @@ public class ExpressionConfigSourceInterceptorTest {
 
     @Test
     public void noExpressionComposed() {
-        SmallRyeConfig config = (SmallRyeConfig) buildConfig("expression", "${my.prop${compose}}");
+        SmallRyeConfig config = buildConfig("expression", "${my.prop${compose}}");
 
         final NoSuchElementException exception = assertThrows(NoSuchElementException.class,
                 () -> config.getValue("expression", String.class));
@@ -79,7 +78,7 @@ public class ExpressionConfigSourceInterceptorTest {
 
     @Test
     public void multipleExpansions() {
-        SmallRyeConfig config = (SmallRyeConfig) buildConfig("my.prop", "1234", "my.prop.two", "${my.prop}", "my.prop.three",
+        SmallRyeConfig config = buildConfig("my.prop", "1234", "my.prop.two", "${my.prop}", "my.prop.three",
                 "${my.prop.two}", "my.prop.four", "${my.prop.three}");
 
         assertEquals("1234", config.getRawValue("my.prop"));
@@ -90,7 +89,7 @@ public class ExpressionConfigSourceInterceptorTest {
 
     @Test
     public void infiniteExpansion() {
-        SmallRyeConfig config = (SmallRyeConfig) buildConfig("my.prop", "${my.prop}");
+        SmallRyeConfig config = buildConfig("my.prop", "${my.prop}");
 
         assertThrows("Recursive expression expansion is too deep for my.prop", IllegalArgumentException.class,
                 () -> config.getRawValue("my.prop"));
@@ -98,7 +97,7 @@ public class ExpressionConfigSourceInterceptorTest {
 
     @Test
     public void withoutExpansion() {
-        SmallRyeConfig config = (SmallRyeConfig) buildConfig("my.prop", "1234", "expression", "${my.prop}");
+        SmallRyeConfig config = buildConfig("my.prop", "1234", "expression", "${my.prop}");
 
         assertEquals("1234", config.getValue("expression", String.class));
 
@@ -109,7 +108,17 @@ public class ExpressionConfigSourceInterceptorTest {
         assertEquals("1234", config.getValue("expression", String.class));
     }
 
-    private static Config buildConfig(String... keyValues) {
+    @Test
+    public void escape() {
+        assertEquals("${my.prop}", buildConfig("expression", "$${my.prop}").getRawValue("expression"));
+
+        assertEquals("file:target/prices/?fileName=${date:now:yyyyMMddssSS}.txt&charset=utf-8",
+                buildConfig("camel.expression",
+                        "file:target/prices/?fileName=$${date:now:yyyyMMddssSS}.txt&charset=utf-8")
+                                .getRawValue("camel.expression"));
+    }
+
+    private static SmallRyeConfig buildConfig(String... keyValues) {
         return new SmallRyeConfigBuilder()
                 .addDefaultSources()
                 .withSources(KeyValuesConfigSource.config(keyValues))
