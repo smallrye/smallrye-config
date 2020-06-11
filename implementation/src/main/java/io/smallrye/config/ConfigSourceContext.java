a13c71be6e3133cc295b0b0f03fc1053760845d5
Roberto Cortez <radcortez@yahoo.com>
2020-06-11 14:19:59 +0100

    Added documentation to ConfigSourceFactory.

--- implementation/src/main/java/io/smallrye/config/ConfigSourceContext.java
+++ implementation/src/main/java/io/smallrye/config/ConfigSourceContext.java
@@ -2,7 +2,10 @@ package io.smallrye.config;
 
 import io.smallrye.common.annotation.Experimental;
 
-@Experimental("")
+/**
+ * Exposes contextual information on the ConfigSource initialization via {@link ConfigSourceFactory}.
+ */
+@Experimental("ConfigSource API Enhancements")
 public interface ConfigSourceContext {
     ConfigValue getValue(String name);
 }
--- implementation/src/main/java/io/smallrye/config/ConfigSourceFactory.java
+++ implementation/src/main/java/io/smallrye/config/ConfigSourceFactory.java
@@ -6,7 +6,22 @@ import org.eclipse.microprofile.config.spi.ConfigSource;
 
 import io.smallrye.common.annotation.Experimental;
 
-@Experimental("")
+/**
+ * This ConfigSourceFactory allows to initialize a {@link ConfigSource}, with access to the current
+ * {@link ConfigSourceContext}.
+ * <p>
+ *
+ * The provided {@link ConfigSource} is initialized in priority order and the current {@link ConfigSourceContext} has
+ * access to all previous initialized {@code ConfigSources}. This allows the factory to configure the
+ * {@link ConfigSource} with configurations read from higher priority {@code ConfigSources}.
+ * <p>
+ *
+ * Instances of this interface will be discovered by {@link SmallRyeConfigBuilder#withSources(ConfigSourceFactory...)}
+ * via the {@link java.util.ServiceLoader} mechanism and can be registered by providing a
+ * {@code META-INF/services/io.smallrye.config.ConfigSourceFactory} which contains the fully qualified class name of the
+ * custom {@link ConfigSourceFactory} implementation.
+ */
+@Experimental("ConfigSource API Enhancements")
 public interface ConfigSourceFactory {
     ConfigSource getSource(ConfigSourceContext context);
 
