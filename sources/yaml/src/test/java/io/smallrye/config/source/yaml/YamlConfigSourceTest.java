c1a0445010254e817192c360d605dde23e251452
Roberto Cortez <radcortez@yahoo.com>
2020-06-18 15:52:40 +0100

    Flatten yaml properties with list and map context.

---
+++ sources/yaml/src/test/java/io/smallrye/config/source/yaml/YamlConfigSourceTest.java
@@ -0,0 +1,84 @@
+package io.smallrye.config.source.yaml;
+
+import static java.util.stream.Collectors.toList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.eclipse.microprofile.config.spi.Converter;
+import org.junit.jupiter.api.Test;
+import org.yaml.snakeyaml.Yaml;
+
+public class YamlConfigSourceTest {
+    @Test
+    void flatten() throws Exception {
+        YamlConfigSource yaml = new YamlConfigSource("yaml",
+                YamlConfigSourceTest.class.getResourceAsStream("/example-216.yml"));
+        String value = yaml.getValue("admin.users");
+        Users users = new UserConverter().convert(value);
+        assertEquals(2, users.getUsers().size());
+        assertEquals(users.users.get(0).getEmail(), "joe@gmail.com");
+        assertEquals(users.users.get(0).getRoles(), Stream.of("Moderator", "Admin").collect(toList()));
+
+        assertEquals("joe@gmail.com", yaml.getValue("admin.users.[0].email"));
+    }
+
+    public static class Users {
+        List<User> users;
+
+        public List<User> getUsers() {
+            return users;
+        }
+
+        public void setUsers(final List<User> users) {
+            this.users = users;
+        }
+    }
+
+    public static class User {
+        String email;
+        String username;
+        String password;
+        List<String> roles;
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(final String email) {
+            this.email = email;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(final String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(final String password) {
+            this.password = password;
+        }
+
+        public List<String> getRoles() {
+            return roles;
+        }
+
+        public void setRoles(final List<String> roles) {
+            this.roles = roles;
+        }
+    }
+
+    static class UserConverter implements Converter<Users> {
+        @Override
+        public Users convert(final String value) {
+            return new Yaml().loadAs(value, Users.class);
+        }
+    }
+}
