c0b7233c58913b0f56b66e3bebaf4e6dffa68999
Roberto Cortez <radcortez@yahoo.com>
2020-07-30 23:48:19 +0100

    Support private fields.

--- implementation/src/test/java/io/smallrye/config/ConfigMappingClassTest.java
+++ implementation/src/test/java/io/smallrye/config/ConfigMappingClassTest.java
@@ -8,9 +8,7 @@ class ConfigMappingClassTest {
     @Test
     void toClass() {
         final Class<?> klass = ConfigMappingClass.toInterface(ServerClass.class);
-        ConfigMappingClass.toInterface(ServerClass.class);
-
-        SmallRyeConfig config = new SmallRyeConfigBuilder().withMapping(klass)
+        final SmallRyeConfig config = new SmallRyeConfigBuilder().withMapping(klass)
                 .withDefaultValue("host", "localhost")
                 .withDefaultValue("port", "8080")
                 .build();
@@ -20,9 +18,35 @@ class ConfigMappingClassTest {
         assertEquals(8080, server.getPort());
     }
 
+    @Test
+    void privateFields() {
+        final Class<?> klass = ConfigMappingClass.toInterface(ServerPrivate.class);
+        final SmallRyeConfig config = new SmallRyeConfigBuilder().withMapping(klass)
+                .withDefaultValue("host", "localhost")
+                .withDefaultValue("port", "8080")
+                .build();
+
+        final ServerPrivate server = config.getConfigMapping(ServerPrivate.class);
+        assertEquals("localhost", server.getHost());
+        assertEquals(8080, server.getPort());
+    }
+
     static class ServerClass {
         String host;
         int port;
+
+        public String getHost() {
+            return host;
+        }
+
+        public int getPort() {
+            return port;
+        }
+    }
+
+    static class ServerPrivate {
+        private String host;
+        private int port;
 
         public String getHost() {
             return host;
