25589a3189d3d12e52c7bf0e1175e8b23a4330d8
Roberto Cortez <radcortez@yahoo.com>
2020-08-20 16:10:06 +0100

    Javadocs for Config Mapping feature.

--- implementation/src/main/java/io/smallrye/config/WithConverter.java
+++ implementation/src/main/java/io/smallrye/config/WithConverter.java
@@ -8,12 +8,15 @@ import java.lang.annotation.Target;
 
 import org.eclipse.microprofile.config.spi.Converter;
 
+import io.smallrye.common.annotation.Experimental;
+
 /**
  * Specify the converter to use to convert the annotated type.
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.METHOD, ElementType.TYPE_USE })
+@Experimental("ConfigMapping API to group configuration properties")
 public @interface WithConverter {
     /**
      * The converter class to use.
--- implementation/src/main/java/io/smallrye/config/WithParentName.java
+++ implementation/src/main/java/io/smallrye/config/WithParentName.java
@@ -6,11 +6,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import io.smallrye.common.annotation.Experimental;
+
 /**
  * Use the parent's configuration name.
  */
 @Documented
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
+@Experimental("ConfigMapping API to group configuration properties")
 public @interface WithParentName {
 }
