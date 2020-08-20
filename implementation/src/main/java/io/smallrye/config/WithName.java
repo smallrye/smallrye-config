e77693d74675b9e2b0ed4d4402fa04357392da03
Roberto Cortez <radcortez@yahoo.com>
2020-08-20 16:10:06 +0100

    Javadocs for Config Mapping feature.

--- implementation/src/main/java/io/smallrye/config/WithName.java
+++ implementation/src/main/java/io/smallrye/config/WithName.java
@@ -6,12 +6,15 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import io.smallrye.common.annotation.Experimental;
+
 /**
  * The name of the configuration property or group.
  */
 @Documented
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
+@Experimental("ConfigMapping API to group configuration properties")
 public @interface WithName {
     /**
      * The name of the property or group. Must not be empty.
