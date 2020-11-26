f5220a0ea5e52ea5e19cc328e90dd04c60f7ba13
Roberto Cortez <radcortez@yahoo.com>
2020-11-26 00:38:44 +0000

    Replace MP Config 2.0 Profile implementation to load microprofile-config.properties file.

--- common/src/main/java/io/smallrye/config/common/utils/ConfigSourceUtil.java
+++ common/src/main/java/io/smallrye/config/common/utils/ConfigSourceUtil.java
@@ -16,15 +16,18 @@
 
 package io.smallrye.config.common.utils;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
 import org.eclipse.microprofile.config.spi.ConfigSource;
+
+import io.smallrye.common.classloader.ClassPathUtils;
 
 /**
  * utilities and constants for {@link ConfigSource} implementations
@@ -55,11 +58,15 @@ public class ConfigSourceUtil {
     }
 
     public static Map<String, String> urlToMap(URL locationOfProperties) throws IOException {
-        try (InputStreamReader reader = new InputStreamReader(locationOfProperties.openStream(), StandardCharsets.UTF_8)) {
-            Properties p = new Properties();
-            p.load(reader);
-            return ConfigSourceUtil.propertiesToMap(p);
-        }
+        final Properties properties = new Properties();
+        ClassPathUtils.consumeStream(locationOfProperties, inputStream -> {
+            try (InputStreamReader reader = new InputStreamReader(locationOfProperties.openStream(), UTF_8)) {
+                properties.load(reader);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        return propertiesToMap(properties);
     }
 
     /**
