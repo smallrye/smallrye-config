8fa8113ce6687f8f68f9ebdcec5a098618cc2b71
Roberto Cortez <radcortez@yahoo.com>
2020-12-04 16:34:51 +0000

    Align YamlConfigSourceProvider with PropertiesConfigSourceProvider.

---
+++ testsuite/extra/src/test/java/io/smallrye/config/test/source/YamlPropertiesTest.java
@@ -0,0 +1,30 @@
+package io.smallrye.config.test.source;
+
+import static org.testng.Assert.assertEquals;
+
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.Config;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.testng.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.testng.annotations.Test;
+
+public class YamlPropertiesTest extends Arquillian {
+    @Deployment
+    public static WebArchive deploy() {
+        return ShrinkWrap.create(WebArchive.class)
+                .addAsResource(new StringAsset("my:\n  yaml: 1234\n"), "META-INF/microprofile-config.yaml")
+                .as(WebArchive.class);
+    }
+
+    @Inject
+    Config config;
+
+    @Test
+    public void yaml() {
+        assertEquals("1234", config.getValue("my.yaml", String.class));
+    }
+}
