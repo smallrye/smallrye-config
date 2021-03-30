563eca01dec9202ef600c37bc1fd90266cc2f8e0
Roberto Cortez <radcortez@yahoo.com>
2021-03-30 18:10:02 +0100

    Don't directly search WEB-INF/classes for config properties files. Missing sources from #528. (#538)

--- sources/hocon/src/main/java/io/smallrye/config/source/hocon/HoconConfigSourceProvider.java
+++ sources/hocon/src/main/java/io/smallrye/config/source/hocon/HoconConfigSourceProvider.java
@@ -13,7 +13,6 @@ import com.typesafe.config.ConfigSyntax;
 public class HoconConfigSourceProvider implements ConfigSourceProvider {
 
     private static final String META_INF_MICROPROFILE_CONFIG_RESOURCE = "META-INF/microprofile-config.conf";
-    private static final String WEB_INF_MICROPROFILE_CONFIG_RESOURCE = "WEB-INF/classes/META-INF/microprofile-config.conf";
 
     static ConfigSource getConfigSource(ClassLoader classLoader, String resource, int ordinal) {
         final Config config = ConfigFactory.parseResourcesAnySyntax(classLoader, resource,
@@ -24,8 +23,7 @@ public class HoconConfigSourceProvider implements ConfigSourceProvider {
     @Override
     public Iterable<ConfigSource> getConfigSources(ClassLoader classLoader) {
         final List<ConfigSource> configSources = new ArrayList<>(2);
-        configSources.add(getConfigSource(classLoader, META_INF_MICROPROFILE_CONFIG_RESOURCE, 60));
-        configSources.add(getConfigSource(classLoader, WEB_INF_MICROPROFILE_CONFIG_RESOURCE, 50));
+        configSources.add(getConfigSource(classLoader, META_INF_MICROPROFILE_CONFIG_RESOURCE, 50));
         return Collections.unmodifiableList(configSources);
     }
 }
