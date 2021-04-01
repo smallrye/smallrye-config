d46d0571734b7a72124fbcf90b00612eeb25e30c
Roberto Cortez <radcortez@yahoo.com>
2021-04-01 11:59:38 +0100

    Add a FileSystemConfigSourceFactory to bootstrap the FileSystemConfigSource with the configuration property smallrye.config.source.file.location.

--- sources/file-system/src/main/java/io/smallrye/config/source/file/FileSystemConfigSourceFactory.java
+++ sources/file-system/src/main/java/io/smallrye/config/source/file/FileSystemConfigSourceFactory.java
@@ -1,4 +1,4 @@
-package io.smallrye.config.examples.configmap;
+package io.smallrye.config.source.file;
 
 import java.util.Collections;
 import java.util.OptionalInt;
@@ -8,14 +8,15 @@ import org.eclipse.microprofile.config.spi.ConfigSource;
 import io.smallrye.config.ConfigSourceContext;
 import io.smallrye.config.ConfigSourceFactory;
 import io.smallrye.config.ConfigValue;
-import io.smallrye.config.source.file.FileSystemConfigSource;
 
-public class ConfigMapConfigSourceFactory implements ConfigSourceFactory {
+public class FileSystemConfigSourceFactory implements ConfigSourceFactory {
+    public static final String SMALLRYE_CONFIG_SOURCE_FILE_LOCATION = "smallrye.config.source.file.location";
+
     @Override
     public Iterable<ConfigSource> getConfigSources(final ConfigSourceContext context) {
-        final ConfigValue value = context.getValue("config.map.dir.source");
+        final ConfigValue value = context.getValue(SMALLRYE_CONFIG_SOURCE_FILE_LOCATION);
         if (value == null || value.getValue() == null) {
-            throw new IllegalArgumentException("CONFIG_MAP_DIR_SOURCE not defined");
+            return Collections.emptyList();
         }
 
         return Collections.singletonList(new FileSystemConfigSource(value.getValue()));
@@ -23,6 +24,6 @@ public class ConfigMapConfigSourceFactory implements ConfigSourceFactory {
 
     @Override
     public OptionalInt getPriority() {
-        return OptionalInt.of(200);
+        return OptionalInt.of(290);
     }
 }
