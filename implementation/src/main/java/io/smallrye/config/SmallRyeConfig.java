97013934cf65860fa8ea828fb337555ed4a5db22
Roberto Cortez <radcortez@yahoo.com>
2021-04-09 18:37:39 +0100

    Interceptors before ConfigSources always.

--- implementation/src/main/java/io/smallrye/config/SmallRyeConfig.java
+++ implementation/src/main/java/io/smallrye/config/SmallRyeConfig.java
@@ -578,6 +578,7 @@ public class SmallRyeConfig implements Config, Serializable {
         private final int priority;
         private final int loadPriority = loadPrioritySequence--;
         private final String name;
+        private final Type type;
 
         private ConfigSourceInterceptor interceptor;
 
@@ -585,12 +586,14 @@ public class SmallRyeConfig implements Config, Serializable {
             this.init = interceptor::getInterceptor;
             this.priority = interceptor.getPriority();
             this.name = "undefined";
+            this.type = Type.INTERCEPTOR;
         }
 
         ConfigSourceInterceptorWithPriority(final ConfigSource configSource) {
             this.init = context -> configSourceInterceptor(configSource);
             this.priority = configSource.getOrdinal();
             this.name = configSource.getName();
+            this.type = Type.CONFIG_SOURCE;
         }
 
         private ConfigSourceInterceptorWithPriority(final ConfigSourceInterceptor interceptor, final int priority,
@@ -599,6 +602,7 @@ public class SmallRyeConfig implements Config, Serializable {
             this.priority = priority;
             this.name = name;
             this.interceptor = interceptor;
+            this.type = Type.INTERCEPTOR;
         }
 
         ConfigSourceInterceptor getInterceptor(final ConfigSourceInterceptorContext context) {
@@ -631,8 +635,19 @@ public class SmallRyeConfig implements Config, Serializable {
 
         @Override
         public int compareTo(final ConfigSourceInterceptorWithPriority other) {
-            int res = Integer.compare(this.priority, other.priority);
-            return res != 0 ? res : Integer.compare(this.loadPriority, other.loadPriority);
+            if (this.type.equals(other.type)) {
+                int res = Integer.compare(this.priority, other.priority);
+                return res != 0 ? res : Integer.compare(this.loadPriority, other.loadPriority);
+            } else if (this.type.equals(Type.INTERCEPTOR)) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+
+        enum Type {
+            INTERCEPTOR,
+            CONFIG_SOURCE;
         }
     }
 
