81a7d2d0e0cd86699931e3cb8d647a30aba4cb8c
Roberto Cortez <radcortez@yahoo.com>
2021-06-25 17:25:25 +0100

    Flatten HoconConfigSource. (#599)

--- sources/hocon/src/main/java/io/smallrye/config/source/hocon/HoconConfigSource.java
+++ sources/hocon/src/main/java/io/smallrye/config/source/hocon/HoconConfigSource.java
@@ -1,18 +1,94 @@
 package io.smallrye.config.source.hocon;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UncheckedIOException;
+import java.net.URL;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
-import com.typesafe.config.Config;
+import org.eclipse.microprofile.config.spi.ConfigSource;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigList;
+import com.typesafe.config.ConfigObject;
+import com.typesafe.config.ConfigValue;
+
+import io.smallrye.common.classloader.ClassPathUtils;
 import io.smallrye.config.common.MapBackedConfigSource;
 
 public class HoconConfigSource extends MapBackedConfigSource {
     private static final long serialVersionUID = -458821383311704657L;
 
+    static final int ORDINAL = ConfigSource.DEFAULT_ORDINAL + 5;
+
+    @Deprecated
     public HoconConfigSource(Config config, String source, int ordinal) {
         super("HoconConfigSource[source=" + source + "]", Collections.unmodifiableMap(config.entrySet().stream()
                 .collect(Collectors.toMap(Map.Entry::getKey, entry -> config.getString(entry.getKey())))), ordinal);
+    }
+
+    public HoconConfigSource(String name, Map<String, String> source, int ordinal) {
+        super(name, source, ordinal, false);
+    }
+
+    public HoconConfigSource(URL url) throws IOException {
+        this(url, ORDINAL);
+    }
+
+    public HoconConfigSource(URL url, int ordinal) throws IOException {
+        this("HoconConfigSource[source=" + url.toString() + "]", ClassPathUtils.readStream(url, inputStream -> {
+            try {
+                return streamToMap(inputStream);
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }), ordinal);
+    }
+
+    private static Map<String, String> streamToMap(InputStream inputStream) throws IOException {
+        final Map<String, String> input = new TreeMap<>();
+        try (Reader reader = new InputStreamReader(inputStream)) {
+            Config config = ConfigFactory.parseReader(reader);
+            input.putAll(configToMap(config));
+        }
+        return input;
+    }
+
+    private static Map<String, String> configToMap(Config config) {
+        final Map<String, String> properties = new TreeMap<>();
+        final Set<Map.Entry<String, ConfigValue>> entries = config.entrySet();
+        for (Map.Entry<String, ConfigValue> entry : entries) {
+            flatten(entry.getKey(), entry.getValue(), properties);
+        }
+        return properties;
+    }
+
+    private static void flatten(String path, ConfigValue value, Map<String, String> target) {
+        if (value instanceof ConfigObject) {
+            flattenObject(path, (ConfigObject) value, target);
+        } else if (value instanceof ConfigList) {
+            flattenList(path, (ConfigList) value, target);
+        } else {
+            target.put(path, value.unwrapped().toString());
+        }
+    }
+
+    private static void flattenObject(String path, ConfigObject value, Map<String, String> target) {
+        for (Map.Entry<String, ConfigValue> entry : value.entrySet()) {
+            flatten(path + "." + entry.getKey(), entry.getValue(), target);
+        }
+    }
+
+    private static void flattenList(String path, ConfigList value, Map<String, String> target) {
+        for (int i = 0, valueSize = value.size(); i < valueSize; i++) {
+            flatten(path + "[" + i + "]", value.get(i), target);
+        }
     }
 }
--- sources/hocon/src/test/java/io/smallrye/config/source/hocon/HoconConfigSourceTest.java
+++ sources/hocon/src/test/java/io/smallrye/config/source/hocon/HoconConfigSourceTest.java
@@ -1,23 +1,91 @@
 package io.smallrye.config.source.hocon;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static io.smallrye.config.SmallRyeConfig.SMALLRYE_CONFIG_LOCATIONS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Iterator;
+import java.util.List;
 
 import org.eclipse.microprofile.config.spi.ConfigSource;
 import org.junit.jupiter.api.Test;
 
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.SmallRyeConfig;
+import io.smallrye.config.SmallRyeConfigBuilder;
+import io.smallrye.config.WithParentName;
+
 class HoconConfigSourceTest {
     @Test
-    void testHocon() {
-        final ConfigSource configSource = HoconConfigSourceProvider.getConfigSource(
-                Thread.currentThread().getContextClassLoader(), "hocon/microprofile-config.conf", 100);
-        assertEquals(HoconConfigSource.DEFAULT_ORDINAL, configSource.getOrdinal());
-        assertArrayEquals(new String[] { "hello.world", "hello.foo.bar" }, configSource.getPropertyNames().toArray());
-        assertArrayEquals(new String[] { "hello.world", "hello.foo.bar" }, configSource.getPropertyNames().toArray());
+    void provider() {
+        Iterator<ConfigSource> iterator = new HoconConfigSourceProvider()
+                .getConfigSources(Thread.currentThread().getContextClassLoader()).iterator();
+        assertTrue(iterator.hasNext());
+
+        ConfigSource configSource = iterator.next();
+        assertEquals(HoconConfigSource.ORDINAL, configSource.getOrdinal());
+        assertTrue(configSource.getPropertyNames().contains("hello.world"));
+        assertTrue(configSource.getPropertyNames().contains("hello.foo.bar"));
         assertEquals("1", configSource.getValue("hello.world"));
         assertEquals("Hell yeah!", configSource.getValue("hello.foo.bar"));
         assertEquals(2, configSource.getProperties().entrySet().size());
         assertEquals("1", configSource.getProperties().get("hello.world"));
         assertEquals("Hell yeah!", configSource.getProperties().get("hello.foo.bar"));
+    }
+
+    @Test
+    void hocon() throws Exception {
+        HoconConfigSource source = new HoconConfigSource(HoconConfigSource.class.getResource(
+                "/META-INF/microprofile-config.conf"));
+        assertEquals(2, source.getPropertyNames().size());
+        assertTrue(source.getPropertyNames().contains("hello.world"));
+        assertTrue(source.getPropertyNames().contains("hello.foo.bar"));
+        assertEquals("1", source.getValue("hello.world"));
+        assertEquals("Hell yeah!", source.getValue("hello.foo.bar"));
+        assertEquals(2, source.getProperties().entrySet().size());
+        assertEquals("1", source.getProperties().get("hello.world"));
+        assertEquals("Hell yeah!", source.getProperties().get("hello.foo.bar"));
+    }
+
+    @Test
+    void list() throws Exception {
+        SmallRyeConfig config = new SmallRyeConfigBuilder()
+                .withSources(new HoconConfigSource(HoconConfigSource.class.getResource("/list.conf")))
+                .withMapping(Countries.class, "countries")
+                .build();
+
+        Countries mapping = config.getConfigMapping(Countries.class);
+        assertFalse(mapping.countries().isEmpty());
+        assertEquals(8, mapping.countries().size());
+        assertEquals("FJ", mapping.countries().get(0).code());
+        assertEquals("Fiji", mapping.countries().get(0).name());
+    }
+
+    @ConfigMapping(prefix = "countries")
+    public interface Countries {
+        @WithParentName
+        List<Country> countries();
+
+        interface Country {
+            String name();
+
+            String code();
+        }
+    }
+
+    @Test
+    void systemFile() {
+        SmallRyeConfig config = buildConfig("./src/test/resources/list.conf");
+
+        assertEquals("FJ", config.getRawValue("countries[0].code"));
+    }
+
+    private static SmallRyeConfig buildConfig(String... locations) {
+        return new SmallRyeConfigBuilder()
+                .addDiscoveredSources()
+                .addDefaultInterceptors()
+                .withDefaultValue(SMALLRYE_CONFIG_LOCATIONS, String.join(",", locations))
+                .build();
     }
 }
