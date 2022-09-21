67b4771b24f2662a20f42ceff1a065eff646d686
Roberto Cortez <radcortez@yahoo.com>
2022-09-21 17:32:22 +0100

    Support an associated mapping in a ConfigSourceFactory

--- implementation/src/main/java/io/smallrye/config/ConfigSourceContext.java
+++ implementation/src/main/java/io/smallrye/config/ConfigSourceContext.java
@@ -1,7 +1,11 @@
 package io.smallrye.config;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+
+import org.eclipse.microprofile.config.spi.ConfigSource;
 
 import io.smallrye.common.annotation.Experimental;
 
@@ -15,4 +19,33 @@ public interface ConfigSourceContext {
     List<String> getProfiles();
 
     Iterator<String> iterateNames();
+
+    class ConfigSourceContextConfigSource implements ConfigSource {
+        private final ConfigSourceContext context;
+
+        public ConfigSourceContextConfigSource(final ConfigSourceContext context) {
+            this.context = context;
+        }
+
+        @Override
+        public Set<String> getPropertyNames() {
+            Set<String> names = new HashSet<>();
+            Iterator<String> namesIterator = context.iterateNames();
+            while (namesIterator.hasNext()) {
+                names.add(namesIterator.next());
+            }
+            return names;
+        }
+
+        @Override
+        public String getValue(final String propertyName) {
+            ConfigValue value = context.getValue(propertyName);
+            return value != null && value.getValue() != null ? value.getValue() : null;
+        }
+
+        @Override
+        public String getName() {
+            return ConfigSourceContextConfigSource.class.getName();
+        }
+    }
 }
