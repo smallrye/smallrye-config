1938a8aa3726ed245aa46feb0f6e02b229c098c8
Roberto Cortez <radcortez@yahoo.com>
2022-11-24 16:47:02 +0000

    Bump Jakarta version to 3.1.x

--- to-jakarta.sh
+++ to-jakarta.sh
@@ -9,8 +9,8 @@ find . -path "*/src/main/resources/META-INF/services/javax*" | sed -e 'p;s/javax
 # docs
 find documentation -type f -name '*.md' -exec sed -i'' -e 's/javax./jakarta./g' {} +
 
-mvn -ntp build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.nextMajorVersion}.0.0-SNAPSHOT
-find examples -maxdepth 1 -type d | xargs -I{} mvn -ntp -pl {} build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.nextMajorVersion}.0.0-SNAPSHOT
+mvn -ntp build-helper:parse-version versions:set -DnewVersion=3.1.0-SNAPSHOT
+find examples -maxdepth 1 -type d | xargs -I{} mvn -ntp -pl {} build-helper:parse-version versions:set -DnewVersion=3.1.0-SNAPSHOT
 
 mvn -ntp versions:update-property -Dproperty=version.eclipse.microprofile.config -DnewVersion=3.0
 #https://issues.sonatype.org/browse/MVNCENTRAL-6872
@@ -19,6 +19,7 @@ sed -i'' -e 's/validation>2.0.2</validation>3.0.1</g' validator/pom.xml
 mvn -ntp versions:update-property -Dproperty=version.hibernate.validator -DnewVersion=7.0.1.Final
 mvn -ntp versions:update-property -Dproperty=version.jakarta.el -DnewVersion=4.0
 mvn -ntp versions:update-property -Dproperty=version.smallrye.common -DnewVersion=2.0.0
-mvn -ntp versions:update-property -Dproperty=version.smallrye.testing.utilities -DnewVersion=2.0.0
+mvn -ntp versions:set-property -Dproperty=version.smallrye.testing -DnewVersion=2.1.0
+mvn -ntp versions:update-property -Dproperty=version.jakarta.json -DnewVersion=2.0.1
 
 mvn versions:set-property -Dproperty=tck.skip.owb -DnewVersion=true
