ff3c4d376d682fd979076733f93d12b68bd7eba9
David M. Lloyd <david.lloyd@redhat.com>
2024-11-27 17:54:39 -0600

    Use module-friendly bundle methods (#1255)

--- sources/zookeeper/src/main/java/io/smallrye/config/source/zookeeper/ZooKeeperMessages.java
+++ sources/zookeeper/src/main/java/io/smallrye/config/source/zookeeper/ZooKeeperMessages.java
@@ -1,12 +1,14 @@
 package io.smallrye.config.source.zookeeper;
 
+import java.lang.invoke.MethodHandles;
+
 import org.jboss.logging.Messages;
 import org.jboss.logging.annotations.Message;
 import org.jboss.logging.annotations.MessageBundle;
 
 @MessageBundle(projectCode = "SRCFG", length = 5)
 interface ZooKeeperMessages {
-    ZooKeeperMessages msg = Messages.getBundle(ZooKeeperMessages.class);
+    ZooKeeperMessages msg = Messages.getBundle(MethodHandles.lookup(), ZooKeeperMessages.class);
 
     @Message(id = 4000, value = "Please set properties for \"" +
             ZooKeeperConfigSource.ZOOKEEPER_URL_KEY + "\" and \"" +
