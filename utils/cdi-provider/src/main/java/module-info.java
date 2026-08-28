783ac8f7821f188671301b8f412bb8b542e87427
Roberto Cortez <radcortez@yahoo.com>
2026-08-28 08:49:01 +0100

    Remove CDI config-source-injection and config-events modules (#1564)

--- utils/cdi-provider/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module io.smallrye.config.util.injection {
-    requires jakarta.annotation;
-    requires jakarta.cdi;
-    requires jakarta.inject;
-
-    requires transitive org.eclipse.microprofile.config;
-
-    exports io.smallrye.config.util.injection;
-}
--- utils/events/src/main/java/io/smallrye/config/events/Type.java
+++
@@ -1,7 +0,0 @@
-package io.smallrye.config.events;
-
-public enum Type {
-    NEW,
-    REMOVE,
-    UPDATE
-}
